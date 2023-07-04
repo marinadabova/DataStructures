851c98d656bf2a4d5a22fa2738ce6f26974c2282
Marina Dabova <107205614+marinadabova@users.noreply.github.com>
2023-07-04 10:24:55 +0300

    Update results.cpp

--- tests/test1_2021/results.cpp
+++ tests/test1_2021/results.cpp
@@ -4,39 +4,42 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-double calculatePoints(const pair<double,double>& a){
+
+double calculatePoints( const pair<double,double>& v){
     double points=0;
-    double br1=a.first;
-    double br2=a.second;
-    points=(br1*br1)/br2;
+    double participants=v.first;
+    double difTeamParticipants=v.second;
+    points=(participants*participants)/difTeamParticipants;
     return points;
 }
-bool sortDescending(const pair<pair<double,double>,int>&a,const pair<pair<double,double>,int>&b){
-    double points1=a.first.first;
-    double points2=b.first.first;
-    double br11=a.first.second;
-    double br12=b.first.second;
-    if(points1==points2){
-        return br11>br12;
+bool sortDescending(const pair<pair<double,double>,int>& a, const pair<pair<double,double>,int>& b){
+    double p1=a.first.first;
+    double p2=b.first.first;
+    double br1=a.first.second;
+    double br2=b.first.second;
+    if(p1==p2){
+        return br1>br2;
+            
     }
-    return points1>points2;
+    return p1>p2;
 }
 int main() {
     int N;
-    cin>>N;
-    vector<pair<double,double>> vec;
-    for(int i=0;i<N;i++){
-        double br1,br2;
-        cin>>br1>>br2;
-        vec.push_back(make_pair(br1,br2));
-    }
+    cin>> N;
     vector<pair<pair<double,double>,int>> result;
+    vector<pair<double,double>> v;
+    
     for(int i=0;i<N;i++){
-        double points=calculatePoints(vec[i]);
-        double br1=vec[i].first;
-        result.push_back(make_pair(make_pair(points,br1),i+1));
+        int num1,num2;
+        cin>>num1>>num2;
+        v.push_back({num1,num2});
     }
-    stable_sort(result.begin(),result.end(),sortDescending);
+    for(int i=0;i<N;i++){
+        double points=calculatePoints(v[i]);
+        double participants=v[i].first;
+        result.push_back({{points,participants},i+1});
+    }
+    stable_sort(result.begin(),result.end(), sortDescending);
     for(int i=0;i<N;i++){
         cout<<result[i].second<<" ";
     }
