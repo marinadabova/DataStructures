ff8e671331861a1a36e677ffcac89c8cab60da0c
Marina Dabova <107205614+marinadabova@users.noreply.github.com>
2023-09-20 15:30:27 +0300

    Update and rename HEAP.cpp to HEAP_stl.cpp

--- Data Structures/Heap/HEAP_stl.cpp
+++ Data Structures/Heap/HEAP_stl.cpp
@@ -1,47 +1,41 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int main() {
 
 	vector<int> v = { 2, 8, 3, 9, 4, 15, 10, 3, 7, 9, 6, 5, 2, 10 };
 
-	if (is_heap(v.begin(), v.end())) cout << "The container is heap ";
-	else    cout << "The container is not heap";
+	if (is_heap(v.begin(), v.end()))
+		cout << "The container is heap ";
+	else
+		cout << "The container is not heap";
 	cout << endl;
 
 	make_heap(v.begin(), v.end()); //convert a range in a container to a heap
    // make_heap(v.begin(), v.end(), greater<int>()); 
 
-	if (is_heap(v.begin(), v.end())) 
+	if (is_heap(v.begin(), v.end()))
 		cout << "The container is heap ";
-	else    
+	else
 		cout << "The container is not heap";
 	cout << endl;
 
 	cout << "The maximum element of heap is : ";
 	cout << v.front() << endl;
 
-
-
-
 	v.push_back(50);
 
 	if (is_heap(v.begin(), v.end()))
 		cout << "The container is heap ";
-	else   
+	else
 		cout << "The container is not heap";
 	cout << endl;
 
 	push_heap(v.begin(), v.end()); //push_heap() to reorder elements
 	//push_heap(v.begin(), v.end(), greater<int>()); 
 
-
 	cout << "The maximum element of heap after push is : ";
 	cout << v.front() << endl;
-
-
-
 
 	pop_heap(v.begin(), v.end()); //delete maximum element
 	v.pop_back();
