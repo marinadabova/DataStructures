11a8254df88fc9616a0738c9204f2da8142b508f
Marina Dabova <107205614+marinadabova@users.noreply.github.com>
2024-05-10 11:44:54 +0300

    change typo

--- Data Structures/sortingAlgorithms/insertionSort.cpp
+++ Data Structures/sortingAlgorithms/insertionSort.cpp
@@ -3,18 +3,17 @@
 using namespace std;
 
 void insertionSort(vector<int>& array) {
-	for (int i = 0; i < array.size(); i++) {
-		int curEl = array[i];
-		int j = i;
-		while (j > 0 && curEl < array[j - 1]) {
-			array[j] = array[j - 1];
-			j--;
-		}
-		array[j] = curEl;
-	}
-	
-
+    for (int i = 1; i < array.size(); i++) {
+        int curEl = array[i];
+        int j = i;
+        while (j > 0 && curEl < array[j - 1]) {
+            array[j] = array[j - 1];
+            j--;
+        }
+        array[j] = curEl;
+    }
 }
+
 int main() {
 
 	vector<int> v = { 5, 7, 3, 1, 6, 2, 7 };
